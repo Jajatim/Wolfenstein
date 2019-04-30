27c38574f00c5049c15947b89798e65458dca43c
Jajatim <35898589+Jajatim@users.noreply.github.com>
2019-04-30 20:23:21 +0200

    Add files via upload

---
+++ Wolf/src/menu/menu.c
@@ -0,0 +1,26 @@
+#include "menu.h"
+
+int menu()
+{
+    /*
+	t_menu m;
+
+	m_init(g, &m);
+
+	while (g->g_state != EXIT)
+	{
+		g->g_state = m_events(g);
+		//m_update(&m, g);
+		m_render(g, &m);
+		if (g->g_state == PLAY)
+		{
+			//m_free(&m);
+			//play(g);
+			//m_init(&m);
+		}
+		SDL_Delay(1);
+	}
+	//m_free(m);
+	return (1);
+    */
+}
---
+++ Wolf/src/menu/menu.h
@@ -0,0 +1,8 @@
+#ifndef GENERIC_H_INCLUDED
+#define GENERIC_H_INCLUDED
+
+#include "generic.h"
+
+int menu();
+
+#endif //GENERIC_H_INCLUDED
